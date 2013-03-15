1d95c94d2ad0f72228fd227ec369f6f169154f34
Spencer McIntyre <zeroSteiner@gmail.com>
2013-03-15 18:15:22 -0400

    Monitor GitHub Pull Requests too.

--- cassie/modules/github.py
+++ cassie/modules/github.py
@@ -21,7 +21,7 @@ class Module(CassieXMPPBotModule):
 
 	def init_bot(self, *args, **kwargs):
 		CassieXMPPBotModule.init_bot(self, *args, **kwargs)
-		self.job_id = self.bot.job_manager.job_add(self.check_repo, None, hours = 0, minutes = 0, seconds = self.check_frequency)
+		self.job_id = self.bot.job_manager.job_add(self.check_repo_activity, None, hours = 0, minutes = 0, seconds = self.check_frequency)
 
 	def config_parser(self, config):
 		self.repositories.append(config.get('repository'))
@@ -51,17 +51,27 @@ class Module(CassieXMPPBotModule):
 			response.append('disabled the github repository monitor')
 		return '\n'.join(response)
 
-	def check_repo(self, *args):
+	def check_repo_activity(self, *args):
+		time_span = datetime.timedelta(0, self.check_frequency)
 		for repository in self.repositories:
 			try:
-				since_timestamp = (datetime.datetime.utcnow() - datetime.timedelta(0, self.check_frequency)).strftime("%Y-%m-%dT%H:%M:%SZ")
+				since_timestamp = (datetime.datetime.utcnow() - time_span).strftime("%Y-%m-%dT%H:%M:%SZ")
 				url_h = urllib2.urlopen('https://api.github.com/repos/' + repository + '/commits?since=' + since_timestamp)
 				data = url_h.read()
 				commits = json.loads(data)
 				if len(commits):
 					self.handle_commits(repository, commits)
 			except:
-				break
+				pass
+			try:
+				url_h = urllib2.urlopen('https://api.github.com/repos/' + repository + '/pulls?state=open')
+				data = url_h.read()
+				pulls = json.loads(data)
+				recent_pulls = filter(lambda pull_rq: (datetime.datetime.strptime(pull_rq['created_at'], "%Y-%m-%dT%H:%M:%SZ") + time_span >= datetime.datetime.utcnow()), pulls)
+				if len(recent_pulls):
+					self.handle_pull_requests(repository, recent_pulls)
+			except:
+				pass
 
 	def handle_commits(self, repository, commits):
 		commits.reverse()
@@ -69,7 +79,18 @@ class Module(CassieXMPPBotModule):
 			commit = commit['commit']
 			committer = commit['committer']['name']
 			message = commit['message'].split('\n')[0]
-			report = "GitHub: {0} pushed to {1} \"{2}\"".format(committer, repository, message)
+			report = "GitHub {0}: {1} pushed commit\"{2}\"".format(repository, committer, message)
+			for room in self.report_rooms:
+				self.bot.join_chat_room(room)
+				self.bot.send_message(room, report, mtype = 'groupchat')
+
+	def handle_pull_requests(self, repository, pull_rqs):
+		pull_rqs.reverse()
+		for pull_rq in pull_rqs:
+			user = pull_rq['user']['login']
+			number = pull_rq['number']
+			title = pull_rq['title']
+			report = "GitHub {0}: {1} opened pull request #{2}\n\"{3}\"".format(repository, user, number, title)
 			for room in self.report_rooms:
 				self.bot.join_chat_room(room)
 				self.bot.send_message(room, report, mtype = 'groupchat')
